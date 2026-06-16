ef3bceb03030fd0854cc32375822c813e5868f88
NixxyHorwitz <nixxy@tontonkuy.dev>
2026-06-16 09:58:30 +0700

    remove: Delete orders.php which was mistakenly copied from another project

--- test_db.php
+++
@@ -1,1 +0,0 @@
-<?php require ""bootstrap.php""; try { $stmt = $pdo->query(""SHOW COLUMNS FROM users""); print_r($stmt->fetchAll(PDO::FETCH_ASSOC)); } catch (Exception $e) { echo $e->getMessage(); } ?>
--- test_db2.php
+++
@@ -1,4 +0,0 @@
-<?php
-require __DIR__ . '/bootstrap.php';
-$stmt = $pdo->query("SHOW COLUMNS FROM referral_commissions");
-print_r(array_column($stmt->fetchAll(PDO::FETCH_ASSOC), 'Field'));
