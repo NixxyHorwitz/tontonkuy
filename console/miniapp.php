b85d7415eeac65682d6e0fde7c340f1ec793fe34
NixxyHorwitz <nixxy@tontonkuy.dev>
2026-06-17 15:31:38 +0700

    fix(admin): correctly pass argument to staff_require in miniapp.php

--- console/miniapp.php
+++ console/miniapp.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 require_once __DIR__ . '/../bootstrap.php';
 require_once __DIR__ . '/auth.php';
-staff_require(); // Membutuhkan login admin
+staff_require('users'); // Membutuhkan login admin
 
 // AJAX Handlers
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
